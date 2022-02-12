7db8af218d20caaeca1effa3c61dcaccaadc39d2
AdvaitaSahu23 <99542286+AdvaitaSahu23@users.noreply.github.com>
2022-02-12 21:13:09 +0530

    Week 1 solutions

---
+++ assignment 1 question no 1/ assignment 1 question no 1/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("NAME: ADVAITA SAHU \n");
+    printf("REGISTRATION NO: 2102020027 \n");
+    printf("BRANCH: CHEMICAL ENGINEERING \n");
+    printf("HOBBIES: DRAWING \n");
+return 0;
+}
